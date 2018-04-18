0b86c833948b98482d20718bfd0da5b09157251d
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2018-04-18 16:12:40 +0900

    make v0.0.1

---
+++ lib/push_more.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "net/https"
+
+class PushMore
+  WEBHOOK_BASE_URL = "https://pushmore.io/webhook/"
+
+  def initialize(body, key: ENV.fetch("PUSH_MORE_KEY"))
+    @body = body
+    @key = key
+  end
+
+  def deliver
+    http = Net::HTTP.new(webhook_uri.host, webhook_uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    request = Net::HTTP::Post.new(webhook_uri.request_uri)
+    request.body = @body
+
+    response = http.request(request)
+
+    if response.body.include? "Error"
+      raise response.body
+    else
+      true
+    end
+  end
+
+  private
+
+  def webhook_uri
+    URI.parse WEBHOOK_BASE_URL + @key
+  end
+end
---
+++ pushmore.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = "pushmore"
+  s.version     = "0.0.1"
+  s.date        = "2018-04-18"
+  s.summary     = "Ruby client for PushMore.io"
+  s.description = "Easily send notifications to Telegram"
+  s.authors     = ["Marc Köhlbrugge"]
+  s.email       = "hey@marckohlbrugge.com"
+  s.files       = ["lib/push_more.rb"]
+  s.homepage    =
+    "http://rubygems.org/gems/pushmore"
+  s.license     = "MIT"
+end
